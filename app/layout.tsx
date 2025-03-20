85f4cdfd3f03ecd7e184873de1fa6c5dfca45eab
pranav-sharma-official <159471319+Pranav-Sharma-Official@users.noreply.github.com>
2025-03-20 22:07:09 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
   title: "Password Strength Checker",
   description: "Check the strength of your password.",
   generator: "Pranav Sharma",
+  icons: './favicon.ico',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
